4c79daee0aa3acb92bb147e2a0f996945ccfb372
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-08-13 16:27:08 -0700

    register page

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -2,13 +2,15 @@
 
 import { useState } from "react";
 import Spinner from "./common/Spinner";
+import PlacesAutocomplete from "react-places-autocomplete";
 
 export default function RegisterForm() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [birthday, setBirthday] = useState("");
-  const [location, setLocation] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
   const [isLoggingIn, setIsLoggingIn] = useState(false);
 
   function handleSubmit(e: React.FormEvent) {
@@ -83,21 +85,38 @@ export default function RegisterForm() {
           }}
         />
       </fieldset>
-      <fieldset className="w-full flex flex-col">
-        <label className="text-xs font-semibold" htmlFor="location">
-          Current location
-        </label>
-        <input
-          className="h-10 m-0 px-2 py-1 border border-slate-200 rounded-md text-sm"
-          type="text"
-          name="location"
-          id="location"
-          value={location}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setLocation(e.target.value);
-          }}
-        />
-      </fieldset>
+      <div className="w-full flex items-center space-x-4">
+        <fieldset className="w-[75%] flex flex-col">
+          <label className="text-xs font-semibold" htmlFor="city">
+            City
+          </label>
+          <input
+            className="h-10 m-0 px-2 py-1 border border-slate-200 rounded-md text-sm"
+            type="text"
+            name="city"
+            id="city"
+            value={city}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setCity(e.target.value);
+            }}
+          />
+        </fieldset>
+        <fieldset className="w-[25%] flex flex-col">
+          <label className="text-xs font-semibold" htmlFor="state">
+            State
+          </label>
+          <input
+            className="h-10 m-0 px-2 py-1 border border-slate-200 rounded-md text-sm"
+            type="text"
+            name="state"
+            id="state"
+            value={state}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setState(e.target.value);
+            }}
+          />
+        </fieldset>
+      </div>
       <button
         className="h-10 w-full flex justify-center items-center rounded-md text-sm bg-black text-white"
         onClick={handleSubmit}
