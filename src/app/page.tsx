705ed464c2107d91a0b04fa0ccf60873e305193f
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-09-25 08:50:23 -0700

    removed vercel reference in footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -160,27 +160,6 @@ export default function Home() {
           </div>
         </section>
       </section>
-
-      <footer className="absolute bottom-3 left-3">
-        <div className="absolute bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 text-xs font-semibold"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="rounded-md"
-              width={50}
-              height={10}
-              priority
-            />
-          </a>
-        </div>
-      </footer>
     </main>
   );
 }
