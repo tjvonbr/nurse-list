0066e652f570b277d2b2a2d19899bd174bf7085d
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-10-11 01:32:23 -0700

    added merge fields to mailchimp endpoint

--- src/app/api/mailchimp/route.ts
+++ src/app/api/mailchimp/route.ts
@@ -3,7 +3,10 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
 const createMailchimpRecordSchema = z.object({
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
   email: z.string(),
+  tags: z.array(z.string()),
 });
 
 const mailchimpKey = process.env.MAILCHIMP_API_KEY || "";
@@ -22,6 +25,11 @@ export async function POST(req: NextRequest) {
     const response = await mailchimp.lists.addListMember(audienceId, {
       email_address: body.email,
       status: "subscribed",
+      ...(body.firstName &&
+        body.lastName && {
+          merge_fields: { FNAME: body.firstName, LNAME: body.lastName },
+        }),
+      tags: body.tags,
     });
 
     return NextResponse.json(response);
@@ -29,9 +37,7 @@ export async function POST(req: NextRequest) {
     if (error instanceof z.ZodError) {
       return new NextResponse(JSON.stringify(error.issues), { status: 422 });
     }
-
     console.log(error);
-
     return new NextResponse(null, { status: 500 });
   }
 }
