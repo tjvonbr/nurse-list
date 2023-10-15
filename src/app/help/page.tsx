87dfbc6d0f16220be751fffcc9459977df8b821e
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-10-15 16:06:32 -0700

    changed positiong of both header and footer

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -1,8 +1,9 @@
 import HelpForm from "@/components/HelpForm";
 
 export default function Help() {
+  const blah = "sm: mt-12 md:mt-24";
   return (
-    <div className="min-h-screen px-5 sm: mt-12 md:mt-24 flex flex-col items-center">
+    <div className="min-h-screen px-5 flex flex-col justify-center items-center">
       <div className="w-full sm:w-3/5 lg:w-2/5 flex flex-col items-center space-y-4">
         <h1 className="text-center text-3xl sm:text-5xl text-black font-extrabold">
           Create a support ticket
