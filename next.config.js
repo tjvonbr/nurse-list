387c270853ddb8780a3b425602019caa7c27a13c
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-10-17 15:08:02 -0700

    remove any instance of google maps

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
 const nextConfig = {
   env: {
     EMAIL_SERVER_PASSWORD: process.env.EMAIL_SERVER_PASSWORD,
-    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
-    OPENWEATHER_API_KEY: process.env.OPENWEATHER_API_KEY,
-    WEATHER_API_KEY: process.env.WEATHER_API_KEY,
-  },
-  images: {
-    domains: ["openweathermap.org"],
   },
 };
 
