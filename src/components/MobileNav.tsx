54fb5394ba9053ee533b35c2c6dee04d89c97d06
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-10-17 02:33:06 -0700

    fixed some padding issues on landing sections

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -10,8 +10,8 @@ interface MobileNavProps {
 
 export default function MobileNav({ items }: MobileNavProps) {
   return (
-    <div className="fixed inset-0 top-16 z-50 grid h-[calc(100vh-4rem)] grid-flow-row auto-rows-max overflow-auto p-6 pb-32 shadow-md animate-in slide-in-from-bottom-80 md:hidden">
-      <div className="relative z-20 grid gap-6 rounded-md bg-white p-4 text-black shadow-md">
+    <div className="fixed inset-0 top-16 z-50 grid h-[calc(100vh-4rem)] grid-flow-row auto-rows-max overflow-auto p-5 pb-32 shadow-md animate-in slide-in-from-bottom-80 md:hidden">
+      <div className="relative z-20 grid gap-3 rounded-md bg-white p-4 text-black shadow-md">
         <Link href="/" className="flex items-center space-x-2">
           <NurseListLogo height={25} width={25} />
           <span className="font-bold">{siteConfig.name}</span>
--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
 export interface InputProps
@@ -7,7 +8,10 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
-        className="h-10 w-full flex-grow px-3 py-2 border border-input rounded-md text-sm text-black placeholder:text-slate-400"
+        className={cn(
+          "h-10 w-full flex-grow px-3 py-2 border border-input rounded-md text-sm text-black placeholder:text-slate-400",
+          className
+        )}
         type={type}
         ref={ref}
         {...props}
